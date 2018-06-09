2ab5f2e99354c486ac786a9d62cefc68efbac0f5
Rodrigo Freitas <rodrigo.freitas@services4you.com.br>
2018-06-09 00:51:22 -0300

    Criar a página de orçamento
    - Criado o roteamento
    - Criado o formulário
    - Trocada as variáveis do nome do app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { OrcamentoComponent } from './pages/orcamento/orcamento.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'sobre', component: AboutComponent},
+  {path: 'orcamento', component: OrcamentoComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full' }
 ]
 
