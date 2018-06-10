037323322615bc09b7bb8c9a29d04bee9583fe53
Rodrigo Freitas <rodrigo.freitas@services4you.com.br>
2018-06-09 22:24:19 -0300

    REsolve erro de roteamento
    - Usado o HashStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
 
 @NgModule({
   exports: [ RouterModule ],
-  imports: [ RouterModule.forRoot(routes)]
+  imports: [ RouterModule.forRoot(routes, {useHash: true})]
 })
 
 export class AppRoutingModule { }
