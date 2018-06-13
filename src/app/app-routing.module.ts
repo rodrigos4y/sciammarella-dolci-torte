c44f22af29a348b7b227ed9d84746ab46b9e3017
Rodrigo Dias <rdias@prazo.org>
2018-06-13 09:19:09 -0300

    Colocar background na home
    - Selecionar imagem de fundo
    - Alterar html para fundo ficar com opacidade
    - Mexe na altura das páginas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { OrcamentoComponent } from './pages/orcamento/orcamento.component';
+import { PedidoComponent } from './pages/pedido/pedido.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'sobre', component: AboutComponent},
   {path: 'orcamento', component: OrcamentoComponent},
+  {path: 'pedido', component: PedidoComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full' }
 ]
 
