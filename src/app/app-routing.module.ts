4d4cba928b7d3cbac341dc06e27197c01f10c41d
Rodrigo Freitas <rodrigo.freitas@services4you.com.br>
2018-06-15 00:20:50 -0300

    Fazer a página dos produtos
    - baixar as imagens de background
    - uso do flat icon e dos ícones do Materialize CSS
    - componente collections -> avatar content do Materialize CSS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { OrcamentoComponent } from './pages/orcamento/orcamento.component';
 import { PedidoComponent } from './pages/pedido/pedido.component';
+import { CatalogoComponent } from './pages/catalogo/catalogo.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'sobre', component: AboutComponent},
   {path: 'orcamento', component: OrcamentoComponent},
   {path: 'pedido', component: PedidoComponent},
+  {path: 'menu', component: CatalogoComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full' }
 ]
 
