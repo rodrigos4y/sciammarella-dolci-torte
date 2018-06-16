e6dcbf8f0875cdeeaf396b63985ac4510f823950
Rodrigo Freitas <rodrigo.freitas@services4you.com.br>
2018-06-16 00:08:23 -0300

    Resolver bug do push
    - Modificado no angular.json o service worker para o da One Signal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,7 @@ import { OrcamentoComponent } from './pages/orcamento/orcamento.component';
 import { PedidoComponent } from './pages/pedido/pedido.component';
 import { NgxMaskModule } from 'ngx-mask';
 import { ReactiveFormsModule } from '@angular/forms';
-import { AngularFireModule } from 'angularfire2';
-// for AngularFireDatabase
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireDatabase } from 'angularfire2/database';
-// for AngularFireAuth
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFireAuth } from 'angularfire2/auth';
+
 
 @NgModule({
   declarations: [
@@ -33,20 +27,10 @@ import { AngularFireAuth } from 'angularfire2/auth';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
+    ServiceWorkerModule.register('/OneSignalSDKWorker.js', { enabled: environment.production }),
     AppRoutingModule,
     NgxMaskModule.forRoot(),
-    ReactiveFormsModule,
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyC23O6pIlVG_dNPSMM5qQ-af4yLAUExauM",
-      authDomain: "sciammarella-dolci-torte.firebaseapp.com",
-    databaseURL: "https://sciammarella-dolci-torte.firebaseio.com",
-    projectId: "sciammarella-dolci-torte",
-    storageBucket: "sciammarella-dolci-torte.appspot.com",
-    messagingSenderId: "974178849528"
-    }),                                       
-    AngularFireDatabaseModule,
-    AngularFireAuthModule  
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
